995bac414857d18b1cf38b3b9c08439b1ee7d269
wicsion <goldin301@gmail.com>
2025-02-09 01:27:54 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,56 @@
+# game.py
+
+from gameparts import Board
+# Добавился ещё один импорт - исключение CellOccupiedError.
+from gameparts import CellOccupiedError, FieldIndexError
+
+
+def main():
+    game = Board()
+    current_player = 'X'
+    running = True
+    game.display()
+
+    while running:
+
+        print(f'Ход делают {current_player}')
+
+        # Запускается бесконечный цикл.
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    # Вот тут выбрасывается новое исключение.
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+
+        game.make_move(row, column, current_player)
+        game.display()
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+if __name__ == '__main__':
+    main() 
